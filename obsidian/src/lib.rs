ef3e9fd237c9ebe3468e7e2458d8389bfdd6c087
Braden Walker <bradenawalker@gmail.com>
2022-06-20 23:26:25 +0800

    put/get into memtable

---
+++ obsidian/src/lib.rs
@@ -0,0 +1,141 @@
+use std::collections::BTreeMap;
+use std::time::SystemTime;
+
+struct Lsm {
+    last_ts: u64,
+    l0: Memtable,
+    levels: Vec<Level>,
+}
+
+impl Lsm {
+    fn new() -> Self {
+        Self {
+            last_ts: 0,
+            l0: Memtable::new(),
+            levels: (0..7).map(|_| Level::new()).collect(),
+        }
+    }
+    fn get(&self, ts: u64, k: &[u8]) -> Option<Vec<u8>> {
+        if let Some((_, v)) = self.l0.get(ts, k) {
+            return match v {
+                Value::Regular(v) => Some(v),
+                Value::Tombstone => None,
+            };
+        }
+        for level in &self.levels {
+            if let Some((_, v)) = level.get(ts, k) {
+                return match v {
+                    Value::Regular(v) => Some(v),
+                    Value::Tombstone => None,
+                };
+            }
+        }
+        None
+    }
+
+    fn put(&mut self, k: Vec<u8>, v: Vec<u8>) -> u64 {
+        let ts = std::cmp::max(
+            self.last_ts + 1,
+            SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .expect("now before UNIX_EPOCH?")
+                .as_nanos() as u64,
+        );
+        self.last_ts = ts;
+        self.l0.put(k, ts, v);
+        ts
+    }
+}
+
+#[derive(Clone)]
+enum Value {
+    Regular(Vec<u8>),
+    Tombstone,
+}
+
+struct Memtable {
+    kvs: BTreeMap<Vec<u8>, BTreeMap<u64, Value>>,
+}
+
+impl Memtable {
+    fn new() -> Self {
+        Self {
+            kvs: BTreeMap::new(),
+        }
+    }
+
+    fn get(&self, ts: u64, k: &[u8]) -> Option<(u64, Value)> {
+        let (record_ts, record_v) = self.kvs.get(k)?.range(0..=ts).next_back()?;
+        Some((*record_ts, record_v.clone()))
+    }
+
+    fn put(&mut self, k: Vec<u8>, ts: u64, v: Vec<u8>) {
+        self.kvs
+            .entry(k)
+            .or_insert(BTreeMap::default())
+            .insert(ts, Value::Regular(v));
+    }
+}
+
+struct Level {
+    // In sorted order by range.
+    runs: Vec<Run>,
+}
+
+impl Level {
+    fn new() -> Self {
+        Self { runs: vec![] }
+    }
+
+    fn get(&self, ts: u64, k: &[u8]) -> Option<(u64, Value)> {
+        // TODO: binary search instead of linear
+        for run in &self.runs {
+            if let Some(r) = run.get(ts, k) {
+                return Some(r);
+            }
+        }
+        None
+    }
+}
+
+struct Run {
+    // Sorted by (k, ts).
+    kvs: Vec<(Vec<u8>, u64, Value)>,
+}
+
+impl Run {
+    fn get(&self, ts: u64, k: &[u8]) -> Option<(u64, Value)> {
+        let entry = match self
+            .kvs
+            .binary_search_by_key(&(k, ts), |entry| (&entry.0, entry.1))
+        {
+            Ok(idx) => &self.kvs[idx],
+            Err(next_idx) if next_idx > 0 => &self.kvs[next_idx - 1],
+            _ => return None,
+        };
+        if entry.0 != k {
+            return None;
+        }
+        Some((entry.1, entry.2.clone()))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Lsm;
+
+    #[test]
+    fn test_put_get() {
+        let mut lsm = Lsm::new();
+        let k = b"abc";
+        let not_k = b"def";
+        let v = b"foo";
+        let write_ts = lsm.put(k.to_vec(), v.to_vec());
+        assert_eq!(lsm.get(write_ts - 1, k), None);
+        assert_eq!(lsm.get(write_ts, k), Some(v.to_vec()));
+        assert_eq!(lsm.get(write_ts + 1, k), Some(v.to_vec()));
+        assert_eq!(lsm.get(write_ts - 1, not_k), None);
+        assert_eq!(lsm.get(write_ts, not_k), None);
+        assert_eq!(lsm.get(write_ts + 1, not_k), None);
+    }
+}
