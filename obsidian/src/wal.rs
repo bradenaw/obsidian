a30e7e3a8d0950a93c57648fb2e35005f8692a5b
Braden Walker <bradenawalker@gmail.com>
2022-09-22 17:59:49 -0400

    wal tests and a couple of fixes

--- obsidian/src/wal.rs
+++ obsidian/src/wal.rs
@@ -20,15 +20,15 @@ struct Wal<E> {
 }
 
 impl<E: Entry + Clone + Send + Sync + 'static> Wal<E> {
-    fn new() -> Self {
+    fn new(buffer_size: u64, buffer_duration: Duration) -> Self {
         let (reqs_send, reqs_recv) = mpsc::channel(1);
         let (highest_seqno_send, highest_seqno_recv) = watch::channel(0);
         let inner = Arc::new(RwLock::new(WalInner::new()));
 
         let process_handle = tokio::spawn(Self::process(
             inner.clone(),
-            65536,
-            Duration::from_millis(5),
+            buffer_size,
+            buffer_duration,
             reqs_recv,
             highest_seqno_send,
         ));
@@ -47,7 +47,9 @@ impl<E: Entry + Clone + Send + Sync + 'static> Wal<E> {
             .send((e, done_send))
             .await
             .map_err(|_| anyhow::anyhow!("Wal processor gone missing"))?;
-        let seqno = done_recv.await?;
+        let seqno = done_recv
+            .await
+            .map_err(|_| anyhow::anyhow!("Wal processor gone missing"))?;
         Ok(seqno)
     }
 
@@ -67,11 +69,12 @@ impl<E: Entry + Clone + Send + Sync + 'static> Wal<E> {
             loop {
                 loop {
                     let inner = self.inner.read().unwrap();
-                    let (min_seqno, max_seqno) = inner.seqno_range();
+                    let (min_seqno, max_seqno_plus_one) = inner.seqno_range();
                     if i < min_seqno {
                         Err(anyhow::anyhow!("fell behind"))?;
                     }
-                    if i > max_seqno {
+                    if i >= max_seqno_plus_one {
+                        // Run off the edge, so must wait for new ones to be written.
                         break;
                     }
                     yield (i, inner.entry(i).unwrap().clone());
@@ -109,10 +112,10 @@ impl<E: Entry + Clone + Send + Sync + 'static> Wal<E> {
                 inner.entries.push_back(entries);
                 (senders, seqno)
             };
+            _ = highest_seqno.send(seqno);
             for sender in senders {
                 _ = sender.send(seqno);
             }
-            _ = highest_seqno.send(seqno);
             *buffer_size = 0;
         }
 
@@ -125,7 +128,7 @@ impl<E: Entry + Clone + Send + Sync + 'static> Wal<E> {
                     }
                     buffer_size += entry_size;
                     buffer.push((entry, sender));
-                    if buffer_size > max_buffer_size {
+                    if buffer_size >= max_buffer_size {
                         flush(&inner, &mut buffer, &mut buffer_size, &mut highest_seqno);
                     }
                     if buffer.len() == 1 {
@@ -182,5 +185,54 @@ impl<E> WalInner<E> {
             self.offset as u64,
             (self.offset + self.entries.len()) as u64,
         )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::task::Poll;
+    use std::time::Duration;
+
+    use futures::future;
+    use futures::stream::StreamExt;
+    use futures::stream::TryStreamExt;
+
+    use super::Entry;
+    use super::Wal;
+
+    impl Entry for u64 {
+        fn size(&self) -> u64 {
+            1
+        }
+    }
+
+    #[tokio::test]
+    async fn test_basic() -> anyhow::Result<()> {
+        let wal = Wal::<u64>::new(3, Duration::from_millis(10000000));
+
+        let mut s = wal.stream(0).boxed_local();
+
+        assert!(matches!(futures::poll!(s.next()), Poll::Pending));
+
+        future::try_join3(wal.append(0), wal.append(1), wal.append(2)).await?;
+
+        assert_eq!(s.try_next().await?, Some((0u64, vec![0u64, 1u64, 2u64])));
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_flush_timeout() -> anyhow::Result<()> {
+        let wal = Wal::<u64>::new(1000, Duration::from_millis(10));
+
+        let mut s = wal.stream(0).boxed_local();
+
+        assert!(matches!(futures::poll!(s.next()), Poll::Pending));
+
+        future::try_join3(wal.append(0), wal.append(1), wal.append(2)).await?;
+
+        assert_eq!(s.try_next().await?, Some((0u64, vec![0u64, 1u64, 2u64])));
+
+        Ok(())
     }
 }
